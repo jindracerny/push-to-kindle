e2d24872ff4de792bb90b4b6afe6c7556f0b1053
Jindra Cerny <jindracerny@gmail.com>
2026-05-07 13:15:26 +0200

    wsgi file edit

--- pythonanywhere_wsgi.py
+++ pythonanywhere_wsgi.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-# Path to your files
-path = '/home/jindracerny/mysite'
+# Path to the current project directory
+path = os.path.dirname(os.path.abspath(__file__))
 if path not in sys.path:
     sys.path.insert(0, path)
 
